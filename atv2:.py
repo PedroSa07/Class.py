931fa0e26d002279400b473648bdacd41771f234
PedroSa07 <130071869+PedroSa07@users.noreply.github.com>
2024-05-22 08:04:35 -0300

    Add files via upload
    aula hj

---
+++ atv2:.py
@@ -0,0 +1,11 @@
+class exercicio:
+    def __init__(self):
+        print('-')
+
+    def exercicio1(self,nome,idade,salario,cargo,turno,setor):
+        print("Meu nome é:",  "Tenho:", "Meu salário é:", "Meu cargo é:", "Meu turno é:", "Meu setor é:")
+
+
+ex = exercicio()
+ex.exercicio1(nome, idade, salario, cargo, turno, setor)
+
