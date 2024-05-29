6fe781ff0cff62f14e4e00e09bd5144a4804b657
PedroSa07 <130071869+PedroSa07@users.noreply.github.com>
2024-05-29 08:43:30 -0300

    Add files via upload

---
+++ Pedro.py
@@ -0,0 +1,25 @@
+class Exercicios:
+
+    def __init__(self):
+        print("-")
+
+    def exercicio1(self,nome_funcionario,idade,salario,cargo,turno,setor):
+        print("Meu nome é",nome_funcionario,"tenho",idade,"recebo" "R$",salario,"meu cargo é",cargo,"trabalho no periodo",turno,"no setor",setor)
+
+
+ex = Exercicios()
+ex.exercicio1(nome_funcionário = "Pedro", idade = 19, salario = "1.900", cargo = "Gerente", turno = "matutino", setor = "RH")
+
+def exercicio2(self,nome_escola,estado,numeros_professores,cidade,numero_militares,logradouro,numero_alunos,):
+    print("O nome da minha escola é:", nome_escola, "Moro no estado do:", estado , "numero_professores:", numero_professores, "Moro na cidade:", cidade, "NumeroMilitares:" , numero_militares,
+         "Na rua:", logradouro, "Numero de alunos:", numero_alunos,   )
+    
+    #exerxicio2
+nome_escola = "Colegio Estadual C.M de Cruzeiro do Oeste"
+estado = "Parana"
+numero_professores = 20
+cidade = "Cruzeiro do Oeste"
+numero_militares = 4
+logradouro = "Rua das Flores"
+numero_alunos = 500
+
